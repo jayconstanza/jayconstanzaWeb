e49810d5111d4db35f82b29563c91b177eeb014e
Jay Constanza <imjayconstanza@gmail.com>
2022-04-07 18:55:23 +0200

    routes added automatically to the nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link v-for="r in $router.options.routes" :key="r.path" :to="r.path">
+      {{ r.name }} |
+    </router-link>
   </nav>
   <router-view />
 </template>
