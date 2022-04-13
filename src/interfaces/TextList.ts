61a792ae4fa5074e4c3b0bcc4eb89466f3b99605
Jay Constanza <imjayconstanza@gmail.com>
2022-04-13 17:10:28 +0200

    added TextList interface

---
+++ src/interfaces/TextList.ts
@@ -0,0 +1,11 @@
+/**
+ * @interface TextList
+ * @description interface that describes translation objects for any text we want to use
+ * example: translation.es.appTitle
+ *
+ */
+export interface TextList {
+  appTitle: string;
+  language: string;
+  claim: string;
+}
