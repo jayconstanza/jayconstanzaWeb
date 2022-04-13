be71d268ab6a2a49c4ec7c0c55d3e2d2450d8201
Jay Constanza <imjayconstanza@gmail.com>
2022-04-13 17:11:58 +0200

    added mplus code font

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=M+PLUS+1+Code:wght@100;200;300;400;500;600;700&display=swap");
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
