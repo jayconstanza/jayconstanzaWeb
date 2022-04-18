04ecf0127177a0c89f2d7dba6aeaea4a90c80bc9
Jay Constanza <imjayconstanza@gmail.com>
2022-04-18 18:36:54 +0200

    Added logic for list rendering in ProfileBox component.
    Data passed from <ProfileBox data="..."> with the IProfileBoxItem structure will be rendered

--- src/components/ProfileBox.vue
+++ src/components/ProfileBox.vue
@@ -1,5 +1,18 @@
-<template>ProfileBox</template>
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in props.data" :key="index">{{ item.title }}</li>
+    </ul>
+  </div>
+</template>
 <style lang="scss" scoped>
 @import "../styles/mixins.scss";
 </style>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { IProfileBoxItem } from "@/interfaces/IProfileBoxItem";
+import { defineProps } from "vue";
+interface Props {
+  data: IProfileBoxItem[];
+}
+const props = defineProps<Props>();
+</script>
