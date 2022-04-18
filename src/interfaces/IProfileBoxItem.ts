2818d2216c132a91065cf908e4d4f4aaa08af496
Jay Constanza <imjayconstanza@gmail.com>
2022-04-18 18:34:59 +0200

    Added ProfileBoxItem interface for items rendered in a ProfileBox component

---
+++ src/interfaces/IProfileBoxItem.ts
@@ -0,0 +1,10 @@
+/**
+ * @interface IProfileBoxItem
+ * @description interface that describes the object passed to the ProfileBox component
+ */
+export interface IProfileBoxItem {
+  iconName?: string;
+  title: string;
+  description?: string;
+  period?: string;
+}
